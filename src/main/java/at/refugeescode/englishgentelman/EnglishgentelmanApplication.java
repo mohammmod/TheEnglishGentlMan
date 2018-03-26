c5b8d7f7f5490d66cb310fa804aed649f7cff57a
mohammmod <mohammad.sawas2@outlook.com>
2018-03-26 10:55:41 +0200

    get started

---
+++ src/main/java/at/refugeescode/englishgentelman/EnglishgentelmanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.englishgentelman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishgentelmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishgentelmanApplication.class, args);
+	}
+}
